8c5f023f92ae08c3ca65b2e1116a949518445ff9
Ayuan100 <369278452@qq.com>
2017-08-21 09:37:15 +0800

    Update test3.js

--- 5.Longest Palindromic Substring/test3.js
+++ 5.Longest Palindromic Substring/test3.js
@@ -1,5 +1,9 @@
 function(){
 //Manacher's algorithm http://www.jianshu.com/p/799bc53d4e3d 
+//1.插空解决奇偶性带来的问题
+//2.避免重复比较：对每一个新字符，
+    //如果有对称的字符，则根据对称字符找到最小长度，再继续比较
+    //没有对称字符，开始比较
 var test = function(nums1, nums2) {
     
 
